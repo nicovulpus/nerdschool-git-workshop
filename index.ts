18a534e88903561843144938c72220e37538f87f
nicovulpus <nicholas.v.vandremo@gmail.com>
2025-04-01 18:42:13 +0200

    Commited changes 2.3-1

--- index.ts
+++ index.ts
@@ -1,8 +1,16 @@
-/**
- * Function that greets a person
- * @param firstname First name
- * @param lastname Last name
- */
+const people = [
+    { firstname: 'Arne', lastname: 'Arnesen' },
+    { firstname: 'Magnus', lastname: 'Magnusson' },
+    { firstname: 'Siri', lastname: 'Sirisen' },
+    { firstname: 'Stefan', lastname: 'Stefanson' },
+]
+
 export const greeting = (firstname: string, lastname: string) => {
     console.log(`Hello ${firstname} ${lastname}`);
-};
+}
+
+export const greetTheGang = () => {
+    people.forEach((person) => {
+        greeting(person.firstname, person.lastname);
+    });
+}
